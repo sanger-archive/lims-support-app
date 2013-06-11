a3718acc292782b306ddfb4bdd6a90d34074e9cb
Matthew Denner <md12@sanger.ac.uk>
2013-06-11 12:52:19 +0100

    Revert "Merge pull request #33 from llh1/51219449_fix_bulk_create_barcode_returned_json"
    This reverts commit 761e8be0027c0b8ff524985522dbd8b297bbd76f, reversing
    changes made to 7abb6de3e85357dc365bafbea5f9b69221a110fb.
    
    Conflicts:
    	Gemfile.lock
    	lib/lims-support-app/version.rb

--- lib/lims-support-app/barcode/create_action_shared.rb
+++ lib/lims-support-app/barcode/create_action_shared.rb
@@ -4,22 +4,24 @@ module Lims::SupportApp
   class Barcode
     module CreateActionShared
 
-      def create_barcode(labware, role, contents, session, number_of_barcode=nil)
+      def create_barcode(labware, role, contents, session, number_of_barcode=1)
         barcodes = []
-        (number_of_barcode || 1).times do
-          barcode = Lims::SupportApp::Barcode.new({
+        number_of_barcode.times do
+          barcode = Lims::SupportApp::Barcode.new(
             :labware  => labware,
             :role     => role,
-            :contents => contents
-          })
+            :contents => contents)
 
           barcode.sanger_code(Barcode::new_barcode)
+
           barcode.ean13_code = barcode.calculate_ean13
+
           session << barcode
-          barcodes << {:barcode => barcode, :uuid => session.uuid_for!(barcode)}
+          
+          barcodes << { :barcode => barcode, :uuid => session.uuid_for!(barcode) }
         end
 
-        number_of_barcode ? {:barcodes => barcodes.map { |b| b[:barcode] }} : barcodes.first
+        number_of_barcode == 1 ? barcodes[0] : { :barcodes => barcodes.map { |b| b[:barcode] } }
       end
     end
   end
