03969d7fe9c9752b558bf6da35680c3eeddbdbf0
Karel <ke4@sanger.ac.uk>
2013-09-17 15:30:58 +0100

    initial label_printer's persistance refactor
    It is not a working version, yet.

--- lib/lims-support-app/label_printer/label_printer_persistor.rb
+++ lib/lims-support-app/label_printer/label_printer_persistor.rb
@@ -1,46 +1,55 @@
-require 'lims-core/persistence/persistor'
+require 'lims-laboratory-app/container_persistor_trait'
 require 'lims-support-app/label_printer/label_printer'
 require 'lims-support-app/label_printer/template'
 
 module Lims::SupportApp
+  # LabelPrinter persistor.
   class LabelPrinter
-    # LabelPrinter persistor.
-    class LabelPrinterPersistor < Lims::Core::Persistence::Persistor
-      Model = LabelPrinter
+
+    (does "lims/laboratory_app/container_persistor",
+      :element          => :template_proxy,
+      :table_name       => :templates,
+      :contained_class  => Template)
+
+    class LabelPrinterPersistor
 
       def filter_attributes_on_save(attributes)
         attributes.delete(:templates)
         attributes
       end
 
-      def save_children(id, label_printer)
-        label_printer["templates"].each do |template|
-          @session.save(template, id)
+      def children_template_proxy(label_printer, children)
+        label_printer.templates.each do |template|
+          template_proxy = TemplateProxy.new(label_printer, label_printer.templates, template)
+          state = @session.state_for(template_proxy)
+          state.resource = template_proxy
+          children << template_proxy
         end
       end
 
-      def load_children(id, label_printer)
-        template.loads(id).each do |template|
-          label_printer.templates << template
-        end
-      end
+      alias template template_proxy
 
-      def template
-        @session.label_printer_template
+      class TemplateProxy
+        SESSION_NAME = :label_printer_template
+
+        def attributes
+          @template.attributes
+        end
+
+        class TemplateProxyPersistor
+
+          def filter_attributes_on_save(attributes, label_printer_id=nil)
+            #TODO label_printer_id?
+            attributes[:label_printer_id] = label_printer_id if label_printer_id
+            attributes
+          end
+
+          def parents_for_attributes(att)
+            []
+          end
+        end
       end
     end
 
-    class Template
-      SESSION_NAME = :label_printer_template
-
-      class TemplatePersistor < Lims::Core::Persistence::Persistor
-        Model = LabelPrinter::Template
-
-        def filter_attributes_on_save(attributes, label_printer_id=nil)
-          attributes[:label_printer_id] = label_printer_id if label_printer_id
-          attributes
-        end
-      end
-    end
   end
 end
