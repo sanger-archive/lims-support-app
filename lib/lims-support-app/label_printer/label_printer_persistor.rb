26ee90a22772ef767801958303ac32f8afc8fb75
Karel <ke4@sanger.ac.uk>
2013-09-18 13:14:24 +0100

    fix the label_printer persistor

--- lib/lims-support-app/label_printer/label_printer_persistor.rb
+++ lib/lims-support-app/label_printer/label_printer_persistor.rb
@@ -15,12 +15,12 @@ module Lims::SupportApp
 
       def filter_attributes_on_save(attributes)
         attributes.delete(:templates)
-        attributes
+        super(attributes)
       end
 
       def children_template_proxy(label_printer, children)
         label_printer.templates.each do |template|
-          template_proxy = TemplateProxy.new(label_printer, label_printer.templates, template)
+          template_proxy = TemplateProxy.new(label_printer, nil, template)
           state = @session.state_for(template_proxy)
           state.resource = template_proxy
           children << template_proxy
@@ -30,23 +30,41 @@ module Lims::SupportApp
       alias template template_proxy
 
       class TemplateProxy
-        SESSION_NAME = :label_printer_template
 
         def attributes
-          @template.attributes
+          (@template.andtap { |t| t.attributes } || {}).tap do |att|
+            att[:label_printer] = @label_printer
+          end
+        end
+
+        def invalid?
+          @label_printer.templates.include?(@template) == false
+        end
+
+        def on_load
+          @label_printer.templates << @template
         end
 
         class TemplateProxyPersistor
 
-          def filter_attributes_on_save(attributes, label_printer_id=nil)
-            #TODO label_printer_id?
-            attributes[:label_printer_id] = label_printer_id if label_printer_id
-            attributes
+          def parents(resource)
+            []
           end
 
           def parents_for_attributes(att)
             []
           end
+
+          def new_from_attributes(attributes)
+            printer = @session.label_printer[attributes.delete(:label_printer_id)]
+            super(attributes) do |att|
+              template = Template.new(att)
+              model.new(printer, nil, template).tap do |proxy|
+                proxy.on_load
+              end
+            end
+          end
+
         end
       end
     end
