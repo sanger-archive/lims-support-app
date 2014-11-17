b615faf4f372ba13db73b9dadbb883a11b6dcfd7
Karel <ke4@sanger.ac.uk>
2014-11-17 16:27:42 +0000

    [SM23] change the default print_count to 0

--- db/migrations/011_add_print_count_to_barcodes_table.rb
+++ db/migrations/011_add_print_count_to_barcodes_table.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table :barcodes do
-      add_column :print_count, Integer, :default => 1
+      add_column :print_count, Integer, :default => 0
     end
   end
 end
